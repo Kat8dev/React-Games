f30453d651af0e6ab621ce0acd1b49223bfee5e5
Khatuna <you@example.com>
2024-02-19 23:14:54 +0100

    start witchcraft game &header responsivness

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,40 +1,93 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { colors } from '../../styles';
+import { useState } from 'react';
 
-const HeaderStyled = styled.header` 
-    background-color: ${colors.primary};
-    color: white;
-    padding: 1rem;   
-    border-bottom: 5px solid ${colors.secondary};
-    nav {
-        ul {
-        display: flex;
-        justify-content: space-around;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        li {
-            a {
-            color: white;
-            text-decoration: none;
-            &:hover {
-                text-decoration: underline;
-            }
-            &.active {
-                text-decoration: underline;
-            }
-            }
+const HeaderStyled = styled.header`
+  background-color: ${colors.primary};
+  color: white;
+  padding: 1rem;
+  border-bottom: 5px solid ${colors.secondary};
+  nav {
+    ul {
+      display: flex;
+      justify-content: space-around;
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      li {
+        a {
+          color: white;
+          text-decoration: none;
+          &:hover {
+            text-decoration: underline;
+          }
+          &.active {
+            text-decoration: underline;
+          }
         }
-        }
+      }
     }
-    `;
+  }
+
+  @media only screen and (max-width: 768px) {
+    nav {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      ul {
+        flex-direction: column;
+
+        li {
+          margin-bottom: 0.5rem;
+        }
+
+        display: none;
+      }
+    }
+  }
+
+  .visible {
+    display: block;
+  }
+`;
+
+const HamburgerIcon = styled.div`
+  display: none;
+  cursor: pointer;
+  width: 100vw;
+  :nth-child(1) {
+    color: inherit;
+    text-decoration: none;
+    margin-left: 1.7rem;
+    font-family: 'Lobster', cursive;
+  }
+  :nth-child(2) {
+    font-size: 2rem;
+  }
+  @media only screen and (max-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <HeaderStyled>
       <nav>
-        <ul>
+        <HamburgerIcon>
+          <Link to="/">GAMES</Link>
+          <div onClick={toggleMenu}>&#9776;</div>
+        </HamburgerIcon>
+
+        <ul className={`${menuOpen && 'visible'}`}>
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
